b0579999737a135ee2adfa00fc40b10f60465ef8
đức lợi phạm <phamducloi919@gmail.com>
2025-06-26 23:39:43 +0700

    fix

--- src/setup/setup.js
+++ src/setup/setup.js
@@ -1,6 +1,6 @@
 // hostlocal tạm thời
 // export const DOMAIN = "http://localhost:3000"
-export const DOMAIN = "http://192.168.60.102:3000"
+// export const DOMAIN = "http://192.168.60.102:3000"
 
 // domain chuẩn sẽ lấy khi đẩy backend lên hosting
-// export const DOMAIN = "https://web-admin-doantotnghiep.onrender.com"
+export const DOMAIN = "https://web-admin-doantotnghiep.onrender.com"
