f01aec168eed0391c92772484c76993134e40045
đức lợi phạm <phamducloi919@gmail.com>
2025-06-27 08:43:09 +0700

    fix: domain

--- src/setup/setup.js
+++ src/setup/setup.js
@@ -3,4 +3,4 @@
 // export const DOMAIN = "http://192.168.60.102:3000"
 
 // domain chuẩn sẽ lấy khi đẩy backend lên hosting
-export const DOMAIN = "https://web-admin-doantotnghiep.onrender.com"
+export const DOMAIN = "https://backend-doantotnghiep-wy5h.onrender.com"
