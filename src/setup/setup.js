ea3ab6e92f08877a466f9f8020d355080fd9e501
đức lợi phạm <phamducloi919@gmail.com>
2025-06-27 21:18:49 +0700

    fix: 2

--- src/setup/setup.js
+++ src/setup/setup.js
@@ -3,4 +3,5 @@
 // export const DOMAIN = "http://192.168.60.102:3000"
 
 // domain chuẩn sẽ lấy khi đẩy backend lên hosting
-export const DOMAIN = "https://backend-doantotnghiep-wy5h.onrender.com"
+// export const DOMAIN = "https://backend-doantotnghiep-wy5h.onrender.com"
+export const DOMAIN = "https://backend-doantotnghiep-1.onrender.com"
