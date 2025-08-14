966f00af59ecd9bb0caf45dc92ffc658ae4be73d
đức lợi phạm <phamducloi919@gmail.com>
2025-08-14 11:46:40 +0700

    fix: vite

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { DOMAIN } from './src/setup/setup'
 
-// https://vite.dev/config/
+const DOMAIN = 'http://160.191.51.75:3000';
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: 5173,
     cors: true,
-    proxy: {
-      '/socket.io': {
-        // target: "https://web-admin-doantotnghiep.onrender.com",
-        target: DOMAIN,
-        ws: true
-      }
-    }
   },
-  base: '/', // Thêm base URL
+  preview: {
+    port: 5173,
+    host: '0.0.0.0'
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    },
+    define: {
+      'process.env.VITE_API_URL': JSON.stringify(DOMAIN)
+    }
+  }
 })
