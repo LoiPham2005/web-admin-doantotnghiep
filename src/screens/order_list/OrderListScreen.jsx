d749298a53d10e99d5d5df38e64d8b9666bf8fb3
đức lợi phạm <phamducloi919@gmail.com>
2025-09-03 21:29:04 +0700

    fix: orderlisst_ui

--- src/screens/order_list/OrderListScreen.jsx
+++ src/screens/order_list/OrderListScreen.jsx
@@ -700,7 +700,7 @@ function OrderListScreen() {
               <th>{t('orderList.discount')}</th>
               <th>{t('orderList.finalTotal')}</th>
               <th>{t('orderList.paymentMethod')}</th>
-              <th>{t('orderList.shippingAddress')}</th>
+              {/* <th>{t('orderList.shippingAddress')}</th> */}
               <th>{t('orderList.orderDate')}</th>
               <th>{t('orderList.orderStatus')}</th>
               <th>{t('orderList.actions')}</th>
@@ -720,7 +720,7 @@ function OrderListScreen() {
                 <td>{order.discount.toLocaleString('vi-VN')}đ</td>
                 <td>{order.final_total.toLocaleString('vi-VN')}đ</td>
                 <td>{order.payment_method}</td>
-                <td>{order.address}</td>
+                {/* <td>{order.address}</td> */}
                 <td>{order.created_at}</td>
                 <td>
                   <span
